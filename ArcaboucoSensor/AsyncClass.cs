5a40d071bfd85f001e068af0121b823293d4f9e7
Dickson Henrique Justino Daniel <ckrique@hotmail.com>
2022-07-08 00:49:53 -0300

    Initial commit

---
+++ ArcaboucoSensor/AsyncClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ApiSensor.ArcaboucoSensor
+{
+    public class AsyncClass
+    {
+        OxygenSaturationSensor sensor = new OxygenSaturationSensor();
+
+        public void StartThread()
+        {
+            Task task = new Task(() =>
+            {
+                while (true)
+                {
+                    sensor.changeSensorValue();
+                    Thread.Sleep(5000);
+                }
+            });
+            task.Start();
+        }
+
+        public double GetSensorValue()
+        {
+            return sensor.oxygenSaturationValue;
+        }
+
+        public void ChangeDirectionSensorValueToUp()
+        {
+            sensor.SetChangingValueDirection(OxygenSaturationSensor.BOTTOM_UP_DIRECTION);
+        }
+
+        public void ChangeDirectionSensorValueToDown()
+        {
+            sensor.SetChangingValueDirection(OxygenSaturationSensor.TOP_DOWN_DIRECTION);
+        }
+
+
+    }
+}
---
+++ ArcaboucoSensor/oxygenSaturationSensor.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApiSensor.ArcaboucoSensor
+{
+    public class OxygenSaturationSensor
+    {
+        public const string TOP_DOWN_DIRECTION = "TOP_DOWN_DIRECTION";
+        public const string BOTTOM_UP_DIRECTION = "BOTTOM_UP_DIRECTION";
+        public const int LOWER_LIMIT  = 19;
+        public const int UPPER_LIMIT = 30; 
+
+
+        public string changingValueDirection { get; set; }
+
+        public string name { get; set; }
+
+        public double oxygenSaturationValue { get; set; }
+
+        public OxygenSaturationSensor()
+        {
+            oxygenSaturationValue = 22.2;
+            changingValueDirection = BOTTOM_UP_DIRECTION;
+        }
+
+        public void SetChangingValueDirection(string direction)
+        {
+            changingValueDirection = direction;
+        }
+
+        public void changeSensorValue()
+        {
+            if (changingValueDirection.Equals(BOTTOM_UP_DIRECTION))
+                increaseSensorValue();
+            else if (changingValueDirection.Equals(TOP_DOWN_DIRECTION))
+                decreaseSensorValue();
+        }
+
+        private void increaseSensorValue()
+        {
+            if(oxygenSaturationValue <= UPPER_LIMIT)
+                oxygenSaturationValue += 0.1;
+        }
+
+        private void decreaseSensorValue()
+        {
+            if (oxygenSaturationValue >= LOWER_LIMIT)                
+                oxygenSaturationValue -= 0.1;
+        }
+    }
+}
