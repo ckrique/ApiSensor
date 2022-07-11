fa07cfa6fa2b846319d79dedd3b464c0fdd73c34
Dickson Henrique Justino Daniel <ckrique@hotmail.com>
2022-07-10 21:20:02 -0300

    Inclusão e ajuste de RestClient que interage com Orion Broker.
    Inclusão e ajuste de RestClient que interage com Orion Broker.
    Envia e recupera valores de lá.

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using FIWARE.OrionClient.IoTAgent;
+using FIWARE.OrionClient.REST;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace ApiSensor.Controllers
@@ -10,9 +13,12 @@ namespace ApiSensor.Controllers
     public class ValuesController : ApiController
     {
         // GET api/values
-        public IEnumerable<string> Get()
+        public async Task<IHttpActionResult> Get()
         {
-            return new string[] { "value1", "value2" };
+            RESTClient<string> restClient = new RESTClient<string>();
+            string sensorEntityName = "urn:ngsi-ld:O2Sat:001";
+            RootDataCatcher sensor = await restClient.GetMeasurementFromBroker(sensorEntityName);
+            return Ok(sensor);
         }
 
         // GET api/values/5
